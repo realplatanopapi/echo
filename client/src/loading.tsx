5fcf6a006108723a19744615c5adb906b78d0d6f
Mario Pabon <me@mariopabon.com>
2019-08-24 14:16:11 -0400

    delay rendering of loading spinner

--- client/src/loading.tsx
+++ client/src/loading.tsx
@@ -1,6 +1,23 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { CenterContent, FillParent } from './styles'
 
 export default function Loading() {
+  const [shouldRender, setShouldRender] = useState(false)
+
+  // make app seem faster by not actually rendering anything until a certain amount of time has passed
+  useEffect(() => {
+    const renderTimeout = setTimeout(() => {
+      setShouldRender(true)
+    }, 150)
+
+    return () => {
+      clearTimeout(renderTimeout)
+    }
+  })
+
+  if (!shouldRender) {
+    return null
+  }
+
   return <CenterContent as={FillParent}>Loading</CenterContent>
 }
