da39f78a0804b9faa1a643ceedb4d1e0195cff47
Mario Pabon <me@mariopabon.com>
2019-08-25 10:57:14 -0400

    Center create post button

--- client/src/board/board.tsx
+++ client/src/board/board.tsx
@@ -1,4 +1,5 @@
 import React, { Suspense, lazy, useState } from 'react'
+import styled, { css } from 'styled-components'
 
 import { Post, Coordinates } from '../types'
 import Posts from './posts'
@@ -7,6 +8,26 @@ import Loading from '../components/loading'
 
 const CreatePostForm = lazy(() => import('./create-post-form'))
 const PostDetails = lazy(() => import('./post-details'))
+
+const CreatePostButton = styled(Button)<CreatePostButtonProps>`
+  align-items: center;
+  display: flex;
+  font-weight: bold;
+  height: 2em;
+  justify-content: center;
+  left: 50%;
+  line-height: 1;
+  position: absolute;
+  top: 50%;
+  transform: translate(-50%, -50%);
+  width: 2.5em;
+
+  ${props =>
+    props.isActive &&
+    css`
+      background: blue;
+    `}
+`
 
 export default function Board(props: BoardProps) {
   const [isCreatingPost, setIsCreatingPost] = useState(false)
@@ -20,13 +41,6 @@ export default function Board(props: BoardProps) {
         }}
       >
         <Suspense fallback={<Loading />}>
-          <Button
-            onClick={() => {
-              setIsCreatingPost(true)
-            }}
-          >
-            create post
-          </Button>
           {isCreatingPost && (
             <CreatePostForm
               coordinates={props.coordinates}
@@ -62,6 +76,15 @@ export default function Board(props: BoardProps) {
           setPostDetails(post)
         }}
       />
+      <CreatePostButton
+        isActive={isCreatingPost}
+        aria-label="create post"
+        onClick={() => {
+          setIsCreatingPost(!isCreatingPost)
+        }}
+      >
+        +
+      </CreatePostButton>
     </>
   )
 }
@@ -70,4 +93,8 @@ interface BoardProps {
   coordinates: Coordinates
   posts: Post[]
   onCreatePost: () => void
+}
+
+interface CreatePostButtonProps {
+  isActive: boolean
 }
